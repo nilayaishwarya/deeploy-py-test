eded2c411efd664e35677af8fff4d245877ad6c4
Lars Suanet <lars@suanet.com>
2021-01-19 16:49:10 +0100

    :art: Change token to access keys

--- deeploy_client_docstrings.py
+++ deeploy_client_docstrings.py
@@ -12,7 +12,9 @@ class Client(object):
     Class containing the Deeploy client options
 
     Attributes:
-      personal_api_token: string representing the client API token
+      access_key: string representing the personal access key
+      secret_key: string representing the personal secret key
+      host: string representing the domain on which Deeploy is hosted
       workspace_id: string representing the workspace id in which to create
         deployments
       local_repository_path: string representing the relative or absolute path
@@ -20,7 +22,9 @@ class Client(object):
       branch_name: string representing the branch name on which to commit. Defaults
         to the local active branch
     """
-    personal_api_token: str
+    access_key: str
+    secret_key: str
+    host: str
     workspace_id: str
     local_repository_path: str
     branch_name: Optional[str]
